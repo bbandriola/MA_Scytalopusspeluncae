23801a49e8cd1ef3889f9cc1262547fe91d4d1cb
bbandriola <78495921+bbandriola@users.noreply.github.com>
2026-08-25 12:07:40 -0300

    Add script for ROH estimation and population processing
    This script runs ROH estimation using bcftools on a specified VCF file and includes commented-out sections for processing population data.

---
+++ Diversity/1_GenerateROHs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# run ROH estimation 
+# com qualidade da base 
+bcftools roh -G30 --output FilteredMinDPMaxDPperInd20MaxMissBialelicSNPs /media/labgenoma5/DATAPART3/bandriola/Scytalopus/snparcher/vcfs/ManuscriptVCFs/FilteredMinDPMaxDPperInd20MaxMissBialelicSNPs_FilteredPCAandUCE_GeographicNames_speluncaecomplex.vcf.gz
+
+#grep "RG" FilteredMinDPMaxDPperInd20MaxMissBialelicSNPs.roh.txt | cut -f 2,3,6 > allsamples.edited.roh.txt
+# add origem populacional 
+#cat scytalopus.popfile
+## SampleID,Pop
+## Sdiamantinensis,Sdiamantinensis
+## SerradaLontras5_lin1,lin1
+
+# get pops + rohs in a single file 
+#awk 'BEGIN{FS="\t";OFS="\t"}NR==FNR{if(FNR>1){split($0,a,",");pop[a[1]]=a[2]}next}FNR==1{print $0,"Population";next}{print $0,(pop[$1]?pop[$1]:"NA")}' \
+#   scytalopus.popfile allsamples.edited.roh.txt > allsamples.edited.roh.population.txt
